cae973f08fc93c01c861facac43895054a5e6f6b
lamouchedu94 <lamouchedu54@gmail.com>
2024-02-20 10:02:23 +0100

    [ADD] Add pages

--- freegg/pages/Home.tsx
+++ freegg/pages/Home.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { useEffect, useState } from 'react';
 import { getAllGames } from '../services/api/request/request';
-import { Games } from '../services/api/types/types';;
+import { Games } from '../services/api/types/types';import Card from '../components/Cards';
+;
 
 export default function Home() {
   const [tabName, setTabName] = useState<Games[]>([])
+  // const [gamesAlea, setGamesAlea] = useState<Games[]>([])
 
   useEffect(() => {
     getAllGames().then((data) => {
@@ -14,36 +16,51 @@ export default function Home() {
     })
   }, [])
   
+  // const
+
   return (
   
-  <ScrollView style={{ flex:1, flexDirection:'column', backgroundColor: "white"}}>
+  <ScrollView style={Globalstyle.container}>
+    
     <Text>
-    <Text>Open up App.tsx to start working on your apaazzssp!</Text>    
       <StatusBar style="auto" />
       {tabName.map((elem, i) => {
-        return <Text key={i}>{elem.title + "\n"}</Text>
+        
+        return (
+
+            <View>
+              <Card title={elem.title} thumbnail={elem.thumbnail} key={i}/>
+              {/* <Card key={i} imagelink={elem.thumbnail}></Card> */}
+            </View>
+          )
       })}
     </Text>
     
   </ScrollView>
 
-//   <View style={styles.container}>
-//       <Text>Open up App.tsx to start working on your apaazzssp!</Text>    
-//       <StatusBar style="auto" />
-//       {tabName.map((elem, i) => {
-//         return <Text key={i}>{elem.title}</Text>
-//       })}
-//     </View>
-
   );
 }
 
 
-const styles = StyleSheet.create({
+const Globalstyle = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    display:'flex',
+    // flexDirection:'column',
+    backgroundColor: "white",
+    width:"auto",
+    borderColor: "red",
+    borderStyle: "solid",
+    borderWidth: 1
+  }
+})
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
+//     flexDirection:'row',
+//     margin:10
+//   },
+// });
