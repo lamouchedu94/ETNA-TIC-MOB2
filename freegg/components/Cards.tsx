eab6ff8b0c3d60333f1a87b4a2ccd3a038a8e8e5
lamouchedu94 <lamouchedu54@gmail.com>
2024-02-21 17:11:54 +0100

    [ADD] Nothing, it dosen't work

--- freegg/components/Cards.tsx
+++ freegg/components/Cards.tsx
@@ -1,26 +1,38 @@
 import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Details from "../pages/Details";
+//import { Link } from 'react-router-dom'
 
 type CardProps = {
     title: string
     thumbnail: string
 }
+const Stack = createNativeStackNavigator()
+
 
 export default function Card({ title, thumbnail }: CardProps) {
     return (
-        <View style={cardStyles.container}>
-            <Image
-                source={{uri: thumbnail }}
-                style={cardStyles.image}
-            />
-            <Text>{title}</Text>
+        <View>
+            <View style={cardStyles.container}>
+                <View>
+                    <Image
+                        source={{uri: thumbnail }}
+                        style={cardStyles.image}
+                    />
+                    <Text>{title}</Text>
+
+                </View>
+            </View>
         </View>
     )
 }
 
 const cardStyles = StyleSheet.create({
     container: {
-      flexDirection:"row",
+      flexDirection:"column",
       flexGrow:1,
       backgroundColor: '#fff',
       width: '100%',
@@ -38,7 +50,10 @@ const cardStyles = StyleSheet.create({
     image: {
         width:100,
         height:100,
-        marginRight:10
-    }
+        marginRight:10,
+        display:'flex',
+        flexDirection:'row'
+    },
+
   });
   
