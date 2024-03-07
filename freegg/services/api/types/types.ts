3f740abf38f04a467c8451b434e9a4a980999557
lamouchedu94 <lamouchedu54@gmail.com>
2024-03-07 09:47:49 +0100

    [MOD] Update

--- freegg/services/api/types/types.ts
+++ freegg/services/api/types/types.ts
@@ -2,6 +2,26 @@ export type Games = {
     id: number
     title: string
     thumbnail: string
+    status: string
     short_description: string
+    description: string
+    game_url: string
     genre: string
-}
+    platform: string
+    publisher: string
+    developer: string
+    release_date: string
+    freetogame_profile_url: string
+    minimum_system_requirements: {
+      os: string
+      processor: string
+      memory: string
+      graphics: string
+      storage: string
+    }
+    screenshots: {
+      id: number
+      image: string
+    }[]
+  }
+  
