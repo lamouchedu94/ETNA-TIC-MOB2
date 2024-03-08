5203518d70269bd790834c3db5fdef7073ece938
lamouchedu94 <lamouchedu54@gmail.com>
2024-03-08 23:08:40 +0100

    [ADD] Correct display for specs

--- freegg/pages/Details.tsx
+++ freegg/pages/Details.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { getSpecificGame } from '../services/api/request/request';
 import { useEffect, useState } from 'react';
 import { Games } from '../services/api/types/types';import Card from '../components/Cards';
-
+import { Linking } from 'react-native';
 
 type GameDetails = {
   title: string
@@ -20,6 +20,8 @@ export default function Details({ route }: { route: { params: { title: string, t
     })
     
   }, [])
+  
+  
   return (
     <ScrollView>
       <View style={imgStyle.container}>
@@ -33,10 +35,19 @@ export default function Details({ route }: { route: { params: { title: string, t
           <Text>{tab?.description}</Text>
           <Text>{tab?.game_url}</Text>
           <Text>Genre : {tab?.genre}</Text>
+          <Text>
+            Minimum system requirements : {"\n"}
+            
+            {tab?.minimum_system_requirements.os ? "os : " + tab?.minimum_system_requirements.os + "\n" : ""}
+            {tab?.minimum_system_requirements.processor ? "processor : " + tab?.minimum_system_requirements.processor + "\n": ""}
+            {tab?.minimum_system_requirements.memory ? "memory : " + tab?.minimum_system_requirements.memory + "\n": ""}
+            {tab?.minimum_system_requirements.graphics ? "Gpu : " + tab?.minimum_system_requirements.graphics + "\n": ""}
+            {tab?.minimum_system_requirements.storage ? "Storage : " + tab?.minimum_system_requirements.storage + "\n" : ""}
+          </Text>
           {/* <Text>{tab?.minimum_system_requirements}</Text> */}
           <Text>Platform : {tab?.platform}</Text>
           <Text>Publisher : {tab?.publisher}</Text>
-          <Text>Release date : {tab?.release_date}</Text>
+          <Text>Release date : {tab?.release_date + "\n"}</Text>
           {tab?.screenshots.map((elem, i) => {
             return (
               <Image
